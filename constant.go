5f051d2a31fb43fa698ba8c77f89a7955885c0a1
lyoz <lyoz@users.noreply.github.com>
2020-09-14 12:11:22 +0900

    implement sending e-mail

---
+++ constant.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	username = "<access key>"
+	password = "<secret access key>"
+	host     = "email-smtp.ap-northeast-1.amazonaws.com"
+	port     = 587
+	from     = "<from email address>"
+	to       = "<to email address>"
+)
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+// username = <access key>
+// password = <secret access key>
+// host     = email-smtp.ap-northeast-1.amazonaws.com
+// port     = 587
+// from     = from email address
+// to       = to email address
+
+func sendMail() {
+	auth := smtp.PlainAuth("", username, password, host)
+
+	msg := []byte("To: " + to + "\r\n" +
+		"Subject: subject test (from smtp)\r\n" +
+		"\r\n" +
+		"This is the message (from smtp)\r\n")
+
+	err := smtp.SendMail(fmt.Sprintf("%s:%d", host, port), auth, from, []string{to}, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	sendMail()
 }

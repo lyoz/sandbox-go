5a93d76bad31beed09c919270ae6f714ffe1c7ca
lyoz <lyoz@users.noreply.github.com>
2020-09-16 11:17:38 +0900

    fix constants declaration

--- constant.go
+++ constant.go
@@ -1,10 +1,18 @@
 package main
 
-const (
-	username = "<access key>"
-	password = "<secret access key>"
-	host     = "email-smtp.ap-northeast-1.amazonaws.com"
-	port     = 587
-	from     = "<from email address>"
-	to       = "<to email address>"
-)
+// SES constants
+var SES = struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	From     string
+	To       string
+}{
+	Username: "<access key>",
+	Password: "<secret access key>",
+	Host:     "email-smtp.ap-northeast-1.amazonaws.com",
+	Port:     587,
+	From:     "<from email address>",
+	To:       "<to email address>",
+}
--- main.go
+++ main.go
@@ -1,43 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/smtp"
 
 	"gopkg.in/gomail.v2"
 )
 
-// username = <access key>
-// password = <secret access key>
-// host     = email-smtp.ap-northeast-1.amazonaws.com
-// port     = 587
-// from     = from email address
-// to       = to email address
-
 func sendMail() {
-	auth := smtp.PlainAuth("", username, password, host)
-
-	msg := []byte("To: " + to + "\r\n" +
-		"Subject: subject test (from smtp)\r\n" +
-		"\r\n" +
-		"This is the message (from smtp)\r\n")
-
-	err := smtp.SendMail(fmt.Sprintf("%s:%d", host, port), auth, from, []string{to}, msg)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func sendMail2() {
 	m := gomail.NewMessage()
 
-	m.SetHeader("From", from)
-	m.SetHeader("To", to)
+	m.SetHeader("From", SES.From)
+	m.SetHeader("To", SES.To)
 	m.SetHeader("Subject", "subject test (from smtp by gomail)")
 	m.SetBody("text/plain", "This is the message (from smtp by gomail)")
 
-	d := gomail.NewDialer(host, port, username, password)
+	d := gomail.NewDialer(SES.Host, SES.Port, SES.Username, SES.Password)
 	err := d.DialAndSend(m)
 	if err != nil {
 		log.Fatal(err)
